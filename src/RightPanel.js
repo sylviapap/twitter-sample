0e9e98c7da1aeca3f99a5b7530a54ae945e23038
Sylvia <sylviapap1@gmail.com>
2020-11-24 22:37:04 -0800

    Add alt text

--- src/RightPanel.js
+++ src/RightPanel.js
@@ -18,7 +18,7 @@ const RightPanel = () => {
       </div>
       <div className="widget-container">
         <h2>What's happening</h2>
-        <img src="https://source.unsplash.com/featured"/>
+        <img src="https://source.unsplash.com/featured" alt="random-image"/>
       </div>
     </div>
   );
