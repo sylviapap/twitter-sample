ac06af012636620e080be5a87c53b622e2ed8000
Sylvia <sylviapap1@gmail.com>
2020-11-24 22:31:52 -0800

    deploy

--- src/Comment.js
+++ src/Comment.js
@@ -1,48 +1,49 @@
-import React from "react";
+import React, { Component } from "react";
 import { Avatar } from "@material-ui/core";
 
-const Comment = ({ comment }) => {
-	const username = comment.email.replace(/[^\w]/g, "").toLowerCase();
-	const colors = [
-		"CornflowerBlue",
-		"Brown",
-		"BlueViolet",
-		"DarkGreen",
-		"DarkGrey",
-		"Coral",
-		"DarkOrange",
-	];
-	let color = colors[Math.floor(Math.random() * colors.length)];
-	return (
-		<div className="post" key>
-			<div className="avatar">
-				<Avatar style={{ backgroundColor: color }}>
-					{comment.email.charAt(0)}
-				</Avatar>
-			</div>
-			<div className="post-body">
-				<div className="post-header">
-					<div className="post-headerText">
-						<h3>
-							<span className="post-headerSpecial">
-								@{username} &bull;{" "}
-								{new Date(
-									+new Date() - Math.floor(Math.random() * 10000000000)
-								).toLocaleDateString()}
-							</span>
-						</h3>
+export default class Comment extends Component {
+	render() {
+    const {comment} = this.props;
+		const username = comment.email.replace(/[^\w]/g, "").toLowerCase();
+		const colors = [
+			"CornflowerBlue",
+			"Brown",
+			"BlueViolet",
+			"DarkGreen",
+			"DarkGrey",
+			"Coral",
+			"DarkOrange",
+		];
+		let color = colors[Math.floor(Math.random() * colors.length)];
+		return (
+			<div className="post">
+				<div className="avatar">
+					<Avatar style={{ backgroundColor: color }}>
+						{comment.email.charAt(0)}
+					</Avatar>
+				</div>
+				<div className="post-body">
+					<div className="post-header">
+						<div className="post-headerText">
+							<h3>
+								<span className="post-headerSpecial">
+									@{username} &bull;{" "}
+									{new Date(
+										+new Date() - Math.floor(Math.random() * 10000000000)
+									).toLocaleDateString()}
+								</span>
+							</h3>
+						</div>
+						<div className="post-headerDescription">{comment.body}</div>
 					</div>
-					<div className="post-headerDescription">{comment.body}</div>
-				</div>
-				<div className="post-footer">
-					<i className="far fa-comment" />
-					<i className="fas fa-retweet" />
-					<i className="far fa-heart" />
-					<i className="fa fa-external-link-alt"></i>
+					<div className="post-footer">
+						<i className="far fa-comment" />
+						<i className="fas fa-retweet" />
+						<i className="far fa-heart" />
+						<i className="fa fa-external-link-alt"></i>
+					</div>
 				</div>
 			</div>
-		</div>
-	);
-};
-
-export default Comment;
+		);
+	}
+}
--- src/Post.js
+++ src/Post.js
@@ -1,63 +1,66 @@
-import React from "react";
+import React, { Component } from "react";
 import { Avatar } from "@material-ui/core";
 import VerifiedUserIcon from "@material-ui/icons/VerifiedUser";
 import { Link } from "react-router-dom";
 
-const Post = ({ post, user, avatar }) => {
-	const username = user.name.replace(/[^\w]/g, "").toLowerCase();
-	const userId = user.id;
-	return (
-		<div className="post" key>
-			<div className="avatar">
-				<Avatar src={avatar.url} />
-			</div>
-			<div className="post-body">
-				<div className="post-header">
-					<div className="post-headerText">
-						<h3>
-							<Link
-								to={{
-									pathname: `/profile/${userId}`,
-								}}
-							>
-								{user.name}{" "}
-							</Link>
-							<span className="post-headerSpecial">
-								<VerifiedUserIcon className="verified" />
+class Post extends Component {
+	render() {
+		const { post, user, avatar } = this.props;
+		const username = user.name.replace(/[^\w]/g, "").toLowerCase();
+		const userId = user.id;
+		return (
+			<div className="post">
+				<div className="avatar">
+					<Avatar src={avatar.url} />
+				</div>
+				<div className="post-body">
+					<div className="post-header">
+						<div className="post-headerText">
+							<h3>
 								<Link
 									to={{
 										pathname: `/profile/${userId}`,
 									}}
 								>
-									{" "}
-									@{username}{" "}
-								</Link>{" "}
-								&bull;{" "}
-								<Link
-									to={{
-										pathname: `/post/${post.id}`,
-									}}
-								>
-									{new Date(
-										+new Date() - Math.floor(Math.random() * 10000000000)
-									).toLocaleDateString()}
+									{user.name}{" "}
 								</Link>
-							</span>
-						</h3>
+								<span className="post-headerSpecial">
+									<VerifiedUserIcon className="verified" />
+									<Link
+										to={{
+											pathname: `/profile/${userId}`,
+										}}
+									>
+										{" "}
+										@{username}{" "}
+									</Link>{" "}
+									&bull;{" "}
+									<Link
+										to={{
+											pathname: `/post/${post.id}`,
+										}}
+									>
+										{new Date(
+											+new Date() - Math.floor(Math.random() * 10000000000)
+										).toLocaleDateString()}
+									</Link>
+								</span>
+							</h3>
+						</div>
+						<div className="post-headerDescription">{post.body}</div>
 					</div>
-					<div className="post-headerDescription">{post.body}</div>
-				</div>
-				<div className="post-footer">
-					<Link to={{ pathname: `/post/${post.id}` }}>
-						<i className="far fa-comment" />
-					</Link>
-					<i className="fas fa-retweet" />
-					<i className="far fa-heart" />
-					<i className="fa fa-external-link-alt"></i>
+					<div className="post-footer">
+						<Link to={{ pathname: `/post/${post.id}` }}>
+							<i className="far fa-comment" />
+						</Link>
+						<i className="fas fa-retweet" />
+						<i className="far fa-heart" />
+						<i className="fa fa-external-link-alt"></i>
+					</div>
 				</div>
 			</div>
-		</div>
-	);
-};
+		);
+	}
+}
 
 export default Post;
