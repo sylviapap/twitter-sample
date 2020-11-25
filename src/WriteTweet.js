3274567308461e4db36ae9b790cb24b81e8ff13d
Sylvia <sylviapap1@gmail.com>
2020-11-24 21:10:17 -0800

    Renaming

--- src/WriteTweet.js
+++ src/WriteTweet.js
@@ -3,13 +3,13 @@ import { Avatar, Button } from "@material-ui/core";
 
 function WriteTweet() {
 	return (
-		<div className="compose-tweet">
+		<div className="write-tweet">
 			<form>
-				<div className="compose-tweet-input">
+				<div className="write-tweet-input">
 					<Avatar src="logo.png" />
 					<input placeholder="What's happening?" type="text" />
 				</div>
-				<Button href="/" type="submit" className="compose-tweet-tweetButton">
+				<Button href="/" type="submit" className="write-tweet-tweetButton">
 					Tweet
 				</Button>
 			</form>
